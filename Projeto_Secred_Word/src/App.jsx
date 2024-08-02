c7af7e20fd802678903c76366954f23a83b92b5a
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2024-08-01 22:28:38 -0300

    criando os componentes do projeto

--- Projeto_Secred_Word/src/App.jsx
+++ Projeto_Secred_Word/src/App.jsx
@@ -1,12 +1,35 @@
+// CSS
 import './App.css'
 
+// React
+import { useCallback, useEffect, useState } from 'react';
+
+// data
+import { wordsList } from "./data/words.js";
+
+// components
 import StartScreen from './components/StartScreen'
+import Game from './components/Game.jsx';
+import GameOver from './components/GameOver.jsx';
+
+const Stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"},
+]
+
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList)
+
+  console.log(words)
 
   return (
     <div className='App'>
-      <StartScreen />
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <Game />}
+      {gameStage === "end" && <GameOver />}
     </div>
   )
 }
