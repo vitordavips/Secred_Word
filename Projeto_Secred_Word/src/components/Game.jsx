8d5138174bd97b71570f2c1ef848048cfd4a8a22
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2024-08-04 17:51:49 -0300

    Estruturando o componente do jogo

--- Projeto_Secred_Word/src/components/Game.jsx
+++ Projeto_Secred_Word/src/components/Game.jsx
@@ -2,9 +2,30 @@ import './Game.css';
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-        <h2>Game</h2>
-        <button onClick={verifyLetter}>finalizar jogo</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Advinhe a palavra:</h1>
+      <h3 className='Dica'>
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <div className="palavraContainer">
+        <span className="letra">A</span>
+        <span className='quadradoEmBranco'></span>
+      </div>
+      <div className="letraContainer">
+        <p>Tente advinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name='letra' maxLength="1" required />
+          <button>jogar!</button>
+        </form>
+      </div>
+      <div className="containerDeLetrasErradas">
+        <p>Letras já utilizadas: </p>
+        <span>a, </span>
+        <span>b, </span>
+      </div>
     </div>
   )
 }
